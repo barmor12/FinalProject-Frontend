bad05efec496af94831a827a7442892b4b9d4dd7
AvielEsperansa <avieles100@gmail.com>
2025-05-25 13:55:17 +0300

    .

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,11 +4,13 @@ export const Colors = {
     text: "#333333", // צבע טקסט כהה
     tint: "#6b4226", // צבע מודגש (Accent)
     tabBarBackground: "#f9f3ea", // צבע רקע של ה-Tabs
+    icon: "#6b4226", // צבע אייקונים
   },
   dark: {
     background: "#121212", // צבע רקע כהה
     text: "#ffffff", // צבע טקסט בהיר
     tint: "#f1c40f", // צבע מודגש (Accent)
     tabBarBackground: "#1f1f1f", // צבע רקע של ה-Tabs
+    icon: "#f1c40f", // צבע אייקונים
   },
 };
