ffd4f2ab18c85d46d980b89b5941dccdd7dcfb58
barmor12 <barmo2@ac.sce.ac.il>
2025-06-11 18:04:53 +0300

    add prod build

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
-// const config = {
-//   BASE_URL: "http://10.0.0.20:3000", // aviel home
-// };
-
 const config = {
-  //BASE_URL: "http://172.20.10.3:3000", // bar home
-  BASE_URL: "http://188.245.37.93:3000", // base server
+  BASE_URL: "https://bakeyapp.com", // SCE server
+
   googleClientIdWeb:
     "382363420678-fhktpquvsaks4olea89uokd73ee6k0ps.apps.googleusercontent.com",
   googleClientIdAndroid:
@@ -12,16 +8,5 @@ const config = {
   googleClientIdIos:
     "382363420678-76qo2sdglhrl24chiiupoalbapq7m67o.apps.googleusercontent.com",
 };
-
-// const config = {
-//   BASE_URL: "http://172.20.10.3:3000", // bar iphone
-// };
-
-// const config = {
-//   BASE_URL: "http://192.168.1.173:3000", // הכתובת של השרת
-// };
-// const config = {
-//   BASE_URL: "http://172.20.10.2:3000", // aviel iphone
-// };
 
 export default config;
